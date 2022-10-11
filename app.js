9f914bd5a520e042bcdcfd0135beb3d743a496b0
miaout11 <miaout.11@gmail.com>
2022-10-11 15:24:08 +0800

    feat: add edit restaurant function

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ app.get('/restaurants/new', (req, res) => {
     return res.render('new')
   })
   app.post('/restaurants', (req, res) => {
-    console.log(req.body)
     Restaurant.create(req.body)
       .then(() => res.redirect('/'))
       .catch(error => console.error(error))
@@ -70,6 +69,33 @@ app.get('/restaurants/:id', (req, res) => {
         .then(restaurant => res.render('show', { restaurant }))
         .catch(error => console.error(error))
 })
+
+// edit page
+app.get('/restaurants/:id/edit', (req, res) => {
+    return Restaurant.findById(req.params.id)
+        .lean()
+        .then(restaurant => res.render('edit', { restaurant }))
+        .catch(error => console.error(error))
+})
+// update restaurant info
+app.post('/restaurants/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+        .then(restaurant => {
+            restaurant.name = req.body.name
+            restaurant.category = req.body.category
+            restaurant.image = req.body.image
+            restaurant.location = req.body.location
+            restaurant.phone = req.body.phone
+            restaurant.google_map = req.body.google_map
+            restaurant.rating = req.body.rating
+            restaurant.description = req.body.description
+            return restaurant.save()
+        })
+        .then(() => res.redirect(`/restaurants/${id}`))
+        .catch(error => console.error(error))
+})
+
 
 // start and listen on the Express server
 app.listen(port, () => {
