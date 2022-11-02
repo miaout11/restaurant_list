ef2f3bc3d8623a37d3891d8c8e51407682ac7a59
miaout11 <miaout.11@gmail.com>
2022-11-02 19:18:15 +0800

    add express-session

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
+const app = express()
 const port = 3000
+
 const routes = require('./routes')
 require('./config/mongoose')
 
-const app = express()
 // express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+// use express-session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // 設定載入靜態檔案
 app.use(express.static('public'))
 // setting body-parser
