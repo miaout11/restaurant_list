4e8c122efb4f99f84904b5c991755282fcafccb0
miaout11 <miaout.11@gmail.com>
2022-11-02 19:51:17 +0800

    add logout feature

--- app.js
+++ app.js
@@ -3,28 +3,38 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
+const routes = require('./routes')
+// 匯入passport設定檔，要寫在 exress-session 後
+const usePassport = require('./config/passport')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
-
-const routes = require('./routes')
-require('./config/mongoose')
 
 // express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-// use express-session
+
+// use express-session 儲存認證結果
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+
 // 設定載入靜態檔案
 app.use(express.static('public'))
+
 // setting body-parser
 app.use(express.urlencoded({ extended: true }))
+
 // use method-override
 app.use(methodOverride('_method'))
-// !!!重構路由器，將 request 導入路由器!!!
+
+// 呼叫 usePassport 函式並傳入參數 app，要寫在路由之前
+usePassport(app)
+
+// 重構路由器，將 request 導入路由器
 app.use(routes)
 
 // start and listen on the Express server
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')
 
 // login page
 router.get('/login', (req, res) => {
   res.render('login')
 })
+// add middleware 驗證 request 登入狀態
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 // register page
 router.get('/register', (req, res) => {
@@ -28,6 +34,12 @@ router.post('/register', (req, res) => {
       }
     })
     .catch(err => console.log(err))
+})
+
+// logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
