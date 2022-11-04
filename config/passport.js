c1444475fae2ab10bc890ab6a7a42e6213ece7a8
miaout11 <miaout.11@gmail.com>
2022-11-04 15:38:02 +0800

    add google auth & delete confirm

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const LocalStratege = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const User = require('../models/user')
 
@@ -32,6 +33,32 @@ module.exports = app => {
     clientID: process.env.FACEBOOK_ID,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }
+  ))
+
+  // 設定google登入策略
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
     profileFields: ['email', 'displayName']
   }, (accessToken, refreshToken, profile, done) => {
     const { name, email } = profile._json
--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -6,9 +6,16 @@ const passport = require('passport')
 router.get('/facebook', passport.authenticate('facebook', {
   scope: ['email', 'public_profile']
 }))
-
-// facebook 把資料發回來
 router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+// 向google發出請求
+router.get('/google', passport.authenticate('google', {
+  scope: ['email', 'profile']
+}))
+router.get('/google/callback', passport.authenticate('google', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
