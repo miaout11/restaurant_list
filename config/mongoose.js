a1000690e08c78829ca8f62c54e89eda019a02ce
miaout11 <miaout.11@gmail.com>
2022-11-05 13:19:07 +0800

    modify new page and mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 const MONGODB_PATH = process.env.MONGODB_PATH
 
 // 設定連線到 mongoDB
-mongoose.connect(MONGODB_PATH, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(MONGODB_PATH, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log('mongode error')
+  console.log('mongodb error')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
